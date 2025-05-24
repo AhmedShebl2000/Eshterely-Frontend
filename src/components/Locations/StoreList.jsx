55711ddd5c03ace82052e2d9fd2ee60a5df6c27e
Youssef <youssefyaser2020@gmail.com>
2025-05-24 05:20:53 +0300

    Refactor StoreList component to conditionally render filtered stores or all stores

--- src/components/Locations/StoreList.jsx
+++ src/components/Locations/StoreList.jsx
@@ -2,20 +2,32 @@ import { useSelector } from "react-redux";
 import StoreCard from "./StoreCard";
 
 const StoreList = ({ setPosition }) => {
-  const stores = useSelector((state) => state.stores);
+  const stores = useSelector((state) => state.stores.stores);
+  const filterdStores = useSelector((state) => state.stores.filterdStores);
 
   return (
     <div className="overflow-y-auto p-4 flex-1 bg-gray-50">
-      {stores.map((store) => (
-        <StoreCard
-          key={store.id}
-          name={store.name}
-          hours={store.hours}
-          address={store.address}
-          position={store.position}
-          setPosition={setPosition}
-        />
-      ))}
+      {filterdStores.length === 0
+        ? stores.map((store) => (
+            <StoreCard
+              key={store.id}
+              name={store.name}
+              hours={store.hours}
+              address={store.address}
+              position={store.position}
+              setPosition={setPosition}
+            />
+          ))
+        : filterdStores.map((store) => (
+            <StoreCard
+              key={store.id}
+              name={store.name}
+              hours={store.hours}
+              address={store.address}
+              position={store.position}
+              setPosition={setPosition}
+            />
+          ))}
     </div>
   );
 };
