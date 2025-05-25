ef88677c356d1a6e51ba25de7b61d1ec2413d3f7
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-05-25 23:18:57 +0300

    for deploy

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -6,7 +6,7 @@ import Loader from "../components/Loader";
 import ErrorFetchingData from "../components/ErrorFetchingData";
 import Products from "../components/products/Products";
 import ProductCategory from "../components/products/subCategory/ProductCategory";
-import SignUp from "../components/ProductDetails/Signup";
+import SignUp from "../components/ProductDetails/SignUp";
 
 export default function ProductsPage() {
   const { category } = useParams();
