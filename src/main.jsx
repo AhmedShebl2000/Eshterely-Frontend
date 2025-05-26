c212c6f567cec9d71c26a800963fe019fc9ef610
Youssef <youssefyaser2020@gmail.com>
2025-05-26 06:36:47 +0300

    Editing and formatting main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+
+import App from "./App.jsx";
+import "./index.css";
+
+// Pages
 import HomePage from "./pages/HomePage.jsx";
 import ProductsPage from "./pages/ProductsPage.jsx";
-import store from "./redux/store.jsx";
-import { Provider } from "react-redux";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import Login from "./pages/Login.jsx";
-
 import ProductDetails, {
   loader as productDetailsLoader,
 } from "./pages/ProductDetails.jsx";
-import Register from "./components/Register/Register.jsx";
 import LocationPage from "./pages/LocationPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
-import { CartProvider } from "./Contexts/CartContext.jsx";
 import ForgorPassword from "./pages/ForgorPassword.jsx";
-import { ForgotPasswordProvider } from "./Contexts/ForgotPasswordContext.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
+// Redux TK
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
+import { CartProvider } from "./Contexts/CartContext.jsx";
+import { ForgotPasswordProvider } from "./Contexts/ForgotPasswordContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createBrowserRouter([
@@ -62,7 +62,7 @@ const router = createBrowserRouter([
         Component: ResetPassword,
       },
       {
-        path: "/About",
+        path: "/about",
         Component: AboutPage,
       },
     ],
