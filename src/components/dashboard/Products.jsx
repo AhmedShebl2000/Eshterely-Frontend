82b62d33685801ae6364a8e42ae25f62b4ca7a05
Youssef <youssefyaser2020@gmail.com>
2025-05-28 20:01:34 +0300

    refactor Products component to load data from API instead of local JSON file

--- src/components/dashboard/Products.jsx
+++ src/components/dashboard/Products.jsx
@@ -1,8 +1,8 @@
 import { DataTable } from "../data-table";
-
-import data from "./data.json";
+import { useLoaderData } from "react-router";
 
 function Products() {
+  const data = useLoaderData();
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
@@ -12,6 +12,50 @@ function Products() {
       </div>
     </div>
   );
+}
+
+export async function loader() {
+  const baseURL = import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN;
+
+  const endpoints = [
+    `${baseURL}/televisions`,
+    `${baseURL}/speakers`,
+    `${baseURL}/soundbars`,
+    `${baseURL}/headphones`,
+  ];
+
+  try {
+    const responses = await Promise.all(endpoints.map((url) => fetch(url)));
+
+    responses.forEach((res, i) => {
+      if (!res.ok)
+        throw new Error(`Failed to fetch from ${endpoints[i]} - ${res.status}`);
+    });
+
+    const allData = await Promise.all(responses.map((res) => res.json()));
+
+    const originalData = allData.flat();
+
+    function transformProducts(products) {
+      return products.map((product) => ({
+        id: product._id,
+        header: product.sub_name || product.name,
+        type:
+          product.category.charAt(0).toUpperCase() + product.category.slice(1),
+        status: product.is_new ? "Coming soon" : "In store",
+        price: `$${product.price}`,
+        quantity: product.category === "televisions" ? "5" : "3",
+        reviewer: "Assign reviewer",
+      }));
+    }
+
+    const transformed = transformProducts(originalData);
+
+    return transformed;
+  } catch (error) {
+    console.error("Error loading products:", error);
+    throw new Error("Failed to load product data");
+  }
 }
 
 export default Products;
