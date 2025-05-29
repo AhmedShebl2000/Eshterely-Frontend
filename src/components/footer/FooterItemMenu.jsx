c97e511a301d93ed766dc6bf7146ed6627ae11f3
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-05-29 22:03:46 +0300

    added link of faq page to footer

--- src/components/footer/FooterItemMenu.jsx
+++ src/components/footer/FooterItemMenu.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router";
+
 function FooterItemMenu({ menuArr }) {
-  console.log(menuArr);
+  const navigate = useNavigate();
+
+  function handleClick(menuItem) {
+    console.log(menuItem);
+    if (menuItem === "FAQ") {
+      navigate("/faq");
+    }
+  }
 
   if (menuArr)
     return menuArr.map((menuItem) => (
-      <div className="mt-2 cursor-pointer text-sm" key={menuItem}>
+      <div
+        onClick={() => handleClick(menuItem)}
+        className="mt-2 cursor-pointer text-sm"
+        key={menuItem}
+      >
         {menuItem}
       </div>
     ));
